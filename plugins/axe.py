850298e44dcb229e61824c50fafbbbcdaf370ac4
eyezis <tabhooked@gmail.com>
2018-03-14 03:41:26 +0200

    use cached entity instead of fetching everytime

--- plugins/axe.py
+++ plugins/axe.py
@@ -6,6 +6,7 @@ from telethon import events
 from telethon.utils import get_peer_id, get_display_name
 
 from kateborg import client
+from kateutil import get_entity_cached
 
 import logging
 logger = logging.getLogger("Kateborg@{}".format(__name__))
@@ -21,7 +22,7 @@ class State:
     def match(self, text):
         return re.match('(?i)^a+nd', text)
 
-    def run(self, event):
+    def run(self, event, peer_id):
         if event.is_reply:
             self.state = 1 if self.match(getattr(event.reply_message, 'message', '')) else 0
             if self.state == 1:
@@ -35,7 +36,9 @@ class State:
         elif self.state == 1:
             if matches and event.message.from_id != self.last_author:
                 event.reply('and my axe!')
-                logger.info('fired in "{}"!'.format(get_display_name(event.chat)))
+                logger.info('fired in "{}"!'.format(
+                    get_display_name(get_entity_cached(peer_id))
+                ))
             self.state = 0
 
         if self.state == 0:
@@ -47,6 +50,6 @@ STATE = defaultdict(State)
 
 @client.on(events.NewMessage)
 def on_message(event):
-    peer_id = get_peer_id(event.chat)
-    if STATE[peer_id].run(event) == State.RESET:
+    peer_id = get_peer_id(event.input_chat)
+    if STATE[peer_id].run(event, peer_id) == State.RESET:
         del STATE[peer_id]
