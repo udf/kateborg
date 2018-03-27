e4fb48f8cc62eac8794122e63929053e958b15d1
eyezis <tabhooked@gmail.com>
2018-03-27 22:17:54 +0200

    get snippets from katestore

--- plugins/snippets.py
+++ plugins/snippets.py
@@ -4,21 +4,30 @@ import re
 from telethon import events, utils
 
 from __main__ import client
+from katestore import Katestore
 
 logger = logging.getLogger(__name__)
 
-snips = {
-    "shrug": "¯\_(ツ)_/¯",
-    "lenny": " ( ͡° ͜ʖ ͡°) ",
-    "look": "ಠ_ಠ",
-    "qtshrug": "^(^.^)^",
-}
+snips = Katestore('snips.json', str)
 
 
-@client.on(events.NewMessage(outgoing=True, pattern=re.compile(r"^!(\w+)$")))
+@client.on(events.NewMessage(outgoing=True, pattern=re.compile(r'^!(\w+)$')))
 def snip(event):
     snippet = snips.get(event.pattern_match.group(1))
     if snippet is not None:
         event.delete()
         client.send_message(event.input_chat, snippet, reply_to=event.message.reply_to_msg_id)
         raise events.StopPropagation
+
+
+@client.on(events.NewMessage(outgoing=True, pattern=re.compile(r'^!snip (\w+)(.*)$')))
+def snip_add(event):
+    name = event.pattern_match.group(1)
+    value = event.pattern_match.group(2).strip()
+
+    if value:
+        snips[name] = value
+    elif name in snips:
+        del snips[name]
+
+    event.delete()
